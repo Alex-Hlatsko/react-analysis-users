38d17ed8b108b52173f1ab94d30d4889fdb45d72
Alen-Berg <alexei.hlatsko@gmail.com>
2023-02-05 18:54:51 +0100

    add JSON data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import {Routes, Route} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+import Home from './pages/Home/Home';
+import Users from  './pages/Users/Users';
+import Analysis from './pages/Analysis//Analysis';
+
 function App() {
+
+  const [usersData, setUsersData] = useState();
+
+  //Get data function
+  const fetchDataJSON = async() => {
+    const URL = "link"
+    await axios.get(URL)
+    .then(res => {
+      console.log(res);
+      return res;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+  
+  useEffect(() => {
+    const users = fetchDataJSON();
+    setUsersData(users);
+  }, [])
+  
+
   return (
-    <>
-      Hello
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/users' element={<Users/>}/>
+      <Route path='/analysis' element={<Analysis/>}/>
+    </Routes>
   );
 }
 
